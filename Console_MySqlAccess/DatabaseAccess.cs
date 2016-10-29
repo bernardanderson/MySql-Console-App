17645179615b8d2654e3c2eb67cb632d92bcd509
Bernard Anderson <bernardanderson@gmail.com>
2016-10-28 22:51:06 -0500

    Setup of MySQL database access, read and write.

---
+++ Console_MySqlAccess/DatabaseAccess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace Console_MySqlAccess
+{
+    class DatabaseAccess
+    {
+        public void connectToDatabase()
+        {
+            string myConnectionString = "server=localhost;uid=root;pwd=;database=hello_test;";
+
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+
+            MySqlCommand newCommand = connection.CreateCommand();
+            newCommand.CommandText = "select * from testnames";
+            connection.Open();
+            MySqlDataReader reader = newCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine("Id: " + reader[0] + " // Name: " + reader[1] + " " + reader[2] + " //Age: " + reader[3]);
+                }
+            connection.Close();
+
+            /*
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex);
+                Console.Read();
+            }
+            */
+            Console.Read();
+
+            newCommand = connection.CreateCommand();
+            newCommand.CommandText = "INSERT INTO testnames (first, last, age) VALUES ('Cordelia', 'Charter', 33)";
+            connection.Open();
+            newCommand.ExecuteNonQuery();
+
+            connection.Close();
+            Console.Read();
+        }
+    }
+}
